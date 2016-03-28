cb52617f24a5b34dbbd99c506e933c2d20de64a1
bingo <xiguaqiezipi@163.com>
2016-03-28 10:18:48 +0800

    add files

---
+++ phalcon_test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bingo
+ * Date: 2016/3/27
+ * Time: 18:05
+ */
+echo "<pre>";
+print_r(get_loaded_extensions());
+echo "</pre>";
+
+
+echo "i will be back!!do you think so ?";
+
+echo phpinfo();
